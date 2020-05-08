871609092cb7ab39292596a105f6cf888ba18a45
Kay <kimjh@bawi.org>
2020-05-08 23:25:22 +0900

    edit postlist page - beta (Not Completed)

--- components/screens/PostListScreen.js
+++ components/screens/PostListScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
+  FlatList,
   SafeAreaView,
   StyleSheet,
   View,
@@ -8,12 +9,36 @@ import {
   StatusBar,
 } from 'react-native';
 
+import ProductImage from '../ProductImage';
+
 const PostListScreen = () => {
+  const [data, setData] = useState([]);
+  const serverURL = 'http://jeonghyunkay.ipdisk.co.kr:8000/list/HDD2/Kay/';
+
+  useEffect(() => {
+    fetch(serverURL + 'post_list.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle='dark-content' backgroundColor='lavender' />
-      <SafeAreaView>
-        <Text style={styles.textHelloWorld}>Post List Page!</Text>
+        <SafeAreaView>
+          <FlatList
+            data={data}
+            keyExtactor={item => item.id}
+            renderItem={({ item }) => (
+              <ProductImage
+                id={item.id}
+                price={item.price}
+                onTouch={() => {}}
+                style={styles.productItem}
+              />
+            )}
+            numColumns={3}
+          />
       </SafeAreaView>
     </View>
   )
@@ -21,14 +46,15 @@ const PostListScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 20,
+    paddingTop: 10,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lavender',
   },
-  textHelloWorld: {
-    fontSize: 30
+  productItem: {
+    backgroundColor: 'coral',
+    padding: 10,
   }
 })
 
