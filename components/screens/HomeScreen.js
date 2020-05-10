def4cc590e04d8cab2d4c791292ce3df3077b4d2
Kay <kimjh@bawi.org>
2020-05-11 01:47:46 +0900

    initialize home screen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,59 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import {
-  Dimensions,
-  FlatList,
   SafeAreaView,
   StyleSheet,
   View,
   Text,
   StatusBar,
-  TouchableOpacity
 } from 'react-native';
 
-import ProductImage from '../ProductImage';
-
-const { width, height } = Dimensions.get('window');
-const SCREEN_WIDTH = width < height ? width : height;
-
 const HomeScreen = ({ navigation }) => {
-  const [data, setData] = useState([]);
-  const serverURL = 'http://jeonghyunkay.ipdisk.co.kr:8000/list/HDD2/Kay/';
-
-  useEffect(() => {
-    fetch(serverURL + 'post_list.json')
-      .then((response) => response.json())
-      .then((json) => setData(json.data))
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <View style={styles.container}>
       <StatusBar barStyle='dark-content' backgroundColor='lavender' />
-        <SafeAreaView>
-          <FlatList
-            data={data}
-            keyExtactor={item => item.id}
-            renderItem={({ item }) => (
-              <View>
-                <TouchableOpacity onPress={() => {
-                  navigation.navigate('PostList', { id: '김철성' })
-                }}>
-                  <Text style={styles.itemNameText}>{item.name}</Text>
-                </TouchableOpacity>
-                <ProductImage
-                  id={item.id}
-                  url={serverURL + 'images/' + item.src}
-                  price={item.price}
-                  onTouch={() => {
-                      // navigation.navigate('ProductDetail', { id: item.id })
-                  }}
-                  style={styles.productItem}
-                  width={SCREEN_WIDTH}
-                />
-              </View>
-            )}
-          />
+      <SafeAreaView>
+        <Text>Home Screen!</Text>
       </SafeAreaView>
     </View>
   )
@@ -66,14 +26,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lavender',
-  },
-  productItem: {
-    paddingBottom: 10,
-    marginBottom: 5,
-    width: SCREEN_WIDTH,
-  },
-  itemNameText: {
-    paddingBottom: 10,
   }
 })
 
