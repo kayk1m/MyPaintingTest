5410f0af97ad58b6cbc30aac219d2da2a999bbbd
Kay <kimjh@bawi.org>
2020-05-11 19:13:54 +0900

    add Profile Pic feature on Profile Page

--- components/screens/ProfileScreen.js
+++ components/screens/ProfileScreen.js
@@ -10,11 +10,13 @@ import {
   Image,
   TouchableWithoutFeedback,
   StatusBar,
+  Button,
 } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const SCREEN_WIDTH = width < height ? width : height;
 const serverURL = 'http://jeonghyunkay.ipdisk.co.kr:8000/list/HDD2/Kay/';
+const PROFILE_IMAGE_SIZE = SCREEN_WIDTH/3.5;
 
 const ProfileScreen = ({ route, navigation }) => {
   const [isLoading, setLoading] = useState(true);
@@ -25,19 +27,13 @@ const ProfileScreen = ({ route, navigation }) => {
   }, []);
 
   const fetchData = async () => {
+    setLoading(true);
     setData(null);
     fetch(serverURL + 'user_list.json')
       .then((response) => response.json())
-      .then((json) => {
-        setData(json.users.find((item, idx) => {
-          return item.user_id === user_id;
-        }).paintings);
-        setLoading(false);
-      })
+      .then((json) => setData(json.users))
       .catch((error) => console.error(error))
-      .finally(() => {
-        console.log('ProfileScreen: data recieved!!');
-      });
+      .finally(() => setLoading(false));
   };
 
   const user_id = route.params.user_id;
@@ -55,11 +51,50 @@ const ProfileScreen = ({ route, navigation }) => {
           style={{ width: SCREEN_WIDTH/3, height: SCREEN_WIDTH/3 }}
         />
       </TouchableWithoutFeedback>
-    )
+    );
   });
 
+  const FanItem = ({ numFans }) => {
+    return (
+      <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}
+        >
+        <View style={{ justifyContent: 'center' }}>
+          <Text>Fan</Text>
+          <Text>{numFans}</Text>
+        </View>
+        <View style={{ justifyContent: 'center' }}>
+          <View style={{
+              width: SCREEN_WIDTH/6,
+              height: SCREEN_WIDTH/12
+            }}
+          >
+            <Button  title='팬 되기' />
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const ProfileHeader = ({ user }) => {
+    return (
+      <View>
+        <View style={{ flexDirection: 'row', padding: 20 }}>
+          <Image
+            source={{ uri: serverURL + 'profile/' + user.profile_image_src }}
+            style={{ width: PROFILE_IMAGE_SIZE, height: PROFILE_IMAGE_SIZE, borderRadius: PROFILE_IMAGE_SIZE/2}}
+          />
+          <FanItem numFans={user.num_fan} />
+        </View>
+        <Text style={{ paddingLeft: 15, paddingBottom: 15 }}>{user.status_msg}</Text>
+      </View>
+    );
+  };
+
   const _handleRefresh = async () => {
-    setLoading(false);
     fetchData();
   };
 
@@ -67,15 +102,27 @@ const ProfileScreen = ({ route, navigation }) => {
     <View style={styles.container}>
       <StatusBar barStyle='dark-content' backgroundColor='lavender' />
         <SafeAreaView>
-          <FlatList
-            data={data}
-            refreshing={isLoading}
-            onRefresh={_handleRefresh}
-            keyExtractor={item => item.painting_id.toString()}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => <Item item={item} />}
-            numColumns={3}
-          />
+          {isLoading
+            ?
+            <Text>Loading ... </Text>
+            :
+            <View>
+              <ProfileHeader user={data.find((item, idx) => {
+                return item.user_id === user_id;
+              })} />
+              <FlatList
+                data={data.find((item, idx) => {
+                  return item.user_id === user_id;
+                }).paintings}
+                refreshing={isLoading}
+                onRefresh={_handleRefresh}
+                keyExtractor={item => item.painting_id.toString()}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => <Item item={item} />}
+                numColumns={3}
+              />
+            </View>
+          }
       </SafeAreaView>
     </View>
   );
